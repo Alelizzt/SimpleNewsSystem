ad23a3014c7adc37e2989ab6789171a9b94f92de
Alelizzt <alejandrolistc@hotmail.com>
2025-05-06 20:18:04 -0500

    Se montan las imagenes de forma estatica desde fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
 from app.routers import news_routes, user_routes
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app.include_router(user_routes.router)
 app.include_router(news_routes.router)
+
+app.mount("/images", StaticFiles(directory="uploads/images"), name="images")
 
 # Usar el atribudo reload=True solo en ambiente de desarrollo
 if __name__ == "__main__":
