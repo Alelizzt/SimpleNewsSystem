df07ba1f620d9178ded11cd42f656703e514bfb7
Alelizzt <alejandrolistc@hotmail.com>
2025-05-06 20:17:26 -0500

    Se implementan distintos formatos de presentacion de los modelos
    similar al patron DTO

--- backend/app/schemas/news.py
+++ backend/app/schemas/news.py
@@ -20,3 +20,19 @@ class News(BaseModel):
     section: Section  # Usa el Enum como tipo
     author: str
     date_published: datetime = datetime.now()
+
+class CreateNews(BaseModel):
+    title: str
+    image: str
+    content: str
+    section: Section  # Usa el Enum como tipo
+    author: int
+    date_published: datetime = datetime.now()
+
+
+class UpdateNews(BaseModel):
+    title: str = None
+    image: str = None
+    content: str = None
+    section: Section = None
+    author: int = None
--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,8 +1,25 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
 class User(BaseModel):
     userame: str
-    email: str
+    email: EmailStr
     password: str
     created_at: datetime = datetime.now()
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UpdateUser(BaseModel):
+    userame: str = None
+    email: EmailStr = None
+    password: str = None
+
+class ShowUser(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    class Config():
+        from_attributes = True # orm_mode
