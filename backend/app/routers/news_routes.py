d3b8e1a0733f81e87cc6cc69bae192434726cf27
Alelizzt <alejandrolistc@hotmail.com>
2025-05-06 20:16:27 -0500

    Se configuran las rutas a los servicios

--- backend/app/routers/news_routes.py
+++ backend/app/routers/news_routes.py
@@ -1,29 +1,62 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, File, HTTPException, UploadFile, Form
+from app.db.database import get_db
+from sqlalchemy.orm import Session
+from app.repository import news_service
+from app.schemas.news import CreateNews, UpdateNews
+from app.db.models import SectionEnum
 
-from app.schemas.news import News
+router = APIRouter(prefix="/news", tags=["News"])
 
-router = APIRouter(
-    prefix="/news",
-    tags=["News"]
-)
+
 @router.post("/")
-def create_news(news: News):
-    print(news)
-    return True
+async def create_news(
+    title: str = Form(...),
+    content: str = Form(...),
+    section: str = Form(...),
+    author: int = Form(...),
+    image: UploadFile = File(...),
+    db: Session = Depends(get_db)
+):
+    try:
+        section_enum = SectionEnum(section)
+    except ValueError:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid section value. Allowed values are: {[e.value for e in SectionEnum]}"
+        )
+
+    news_data = CreateNews(
+        title=title,
+        content=content,
+        section=section_enum,
+        author=author,
+        image=image.filename
+    )
+    return news_service.create_news(news_data, image, db)
 
 @router.get("/")
-def get_all_news():
-    return {"message": "Get all news"}
+def get_all_news(db: Session = Depends(get_db)):
+    current_news = news_service.get_all_news(db)
+    return current_news
+
 
 @router.get("/{news_id}")
-def get_news(news_id: int):
-    return {"message": f"Get news with id {news_id}"}
+def get_news(news_id: int, db:Session = Depends(get_db)):
+    news_found = news_service.get_news(news_id, db)
+    return news_found
+
 
 @router.delete("/{news_id}")
-def delete_news(news_id:int):
-    return True
+def delete_news(news_id: int, db: Session = Depends(get_db)):
+    response = news_service.delete_news(news_id, db)
+    return response
 
-@router.patch('/{news_id}')
-def update_news(news_id:int, update_new:News,):
-    #update_user(news_id,update_new)
-    return True
+
+@router.patch("/{news_id}")
+def update_news(
+    news_id: int,
+    update_new: UpdateNews,
+    db:Session = Depends(get_db),
+):
+    response = news_service.update_news(news_id, update_new, db)
+    return response
--- backend/app/routers/user_routes.py
+++ backend/app/routers/user_routes.py
@@ -2,34 +2,37 @@ from fastapi import APIRouter, Depends
 from app.db.database import get_db
 from sqlalchemy.orm import Session
 from app.repository import user_service
-from app.schemas.user import User
+from app.schemas.user import ShowUser, UpdateUser, UserCreate
+from typing import List
 
-router = APIRouter(
-    prefix="/users",
-    tags=["Users"]
-)
+router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.get("/")
-def get_all_users(db:Session = Depends(get_db)):
+
+@router.get("/", response_model=List[ShowUser])
+def get_all_users(db: Session = Depends(get_db)):
     current_users = user_service.get_all_users(db)
     return current_users
 
-@router.post("/")
-def create_user(new_user:User, db:Session = Depends(get_db)):
-    user_service.create_user(new_user, db)
-    return {"response":"User created successfully!"}
 
-@router.get("/{user_id}")
-def get_user(user_id: int, db:Session = Depends(get_db)):
+@router.post("/")
+def create_new_user(new_user: UserCreate, db: Session = Depends(get_db)):
+    user_service.create_user(new_user, db)
+    return {"response": "User created successfully!"}
+
+
+@router.get("/{user_id}", response_model=ShowUser)
+def get_user(user_id: int, db: Session = Depends(get_db)):
     user_found = user_service.get_user(user_id, db)
     return user_found
 
+
 @router.delete("/{user_id}")
-def delete_user(user_id:int , db:Session = Depends(get_db)):
+def delete_user(user_id: int, db: Session = Depends(get_db)):
     response = user_service.delete_user(user_id, db)
     return response
 
-@router.patch('/{user_id}')
-def update_user(user_id:int, updateUser:User, db:Session = Depends(get_db)):
+
+@router.patch("/{user_id}")
+def update_user(user_id: int, updateUser: UpdateUser, db: Session = Depends(get_db)):
     response = user_service.update_user(user_id, updateUser, db)
     return response
