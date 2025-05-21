7369d5ff63f8108c4bd4041369357da8a5193deb
Alelizzt <alejandrolistc@hotmail.com>
2025-05-21 18:55:34 -0500

    Se añade la version de la api en la request

--- backend/app/oauth.py
+++ backend/app/oauth.py
@@ -1,8 +1,10 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from app.token import verify_token
+from app.core.config import settings
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+API_VERSION_PREFIX = f"/v{settings.PROJECT_VERSION.split('.')[0]}"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{API_VERSION_PREFIX}/login")
 
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
